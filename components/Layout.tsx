956fe3dd1f47e03c6d07756adb0df86aff7dd909
Your Name <you@example.com>
2024-12-14 22:55:46 +0200

    commit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,6 +11,7 @@ import LanguageSelector from './LanguageSelector'
 export default function Layout({ children }: { children: React.ReactNode }) {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
 // hello world
+console.log("hello world")
   useEffect(() => {
     if (isMobileMenuOpen) {
       document.body.style.overflow = 'hidden';
